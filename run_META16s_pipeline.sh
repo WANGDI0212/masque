8d0e1507075bf40c0dbc8af5d2bf84aae266ea68
Amine Ghozlane <amine.ghozlane@pasteur.fr>
2016-02-18 11:26:34 +0100

    debug last size

--- run_META16s_pipeline.sh
+++ run_META16s_pipeline.sh
@@ -22,7 +22,7 @@ source /local/gensoft2/adm/etc/profile.d/modules.sh
 module purge
 module add Python/2.7.8 FastTree/2.1.8 FLASH/1.2.11 fasta mafft/7.149 bowtie2/2.2.3 blast+/2.2.31 vsearch/1.4.1 AlienTrimmer/0.4.0 fastqc/0.11.2 rdp_classifier/2.11 swarm/2.1.1
 
-/bin/bash $HOME/META16s_pipeline/META16s_pipeline.sh -i $1/ -o $2/ -n $3 -t $4 --minoverlap 10 --maxoverlap 550 -b &> $2/stat_process.txt
+/bin/bash $HOME/META16s_pipeline/META16s_pipeline.sh -i $1/ -o $2/ -n $3 -t $4 --minoverlap 10 --maxoverlap 550 -b &> $2/${3}_stat_process.txt
 """>$PBS_SCRIPT
 PBSID=`qsub $PBS_SCRIPT`
 echo "! Soumission PBS :> JOBID = $PBSID"
--- run_META16s_pipeline_amplicon.sh
+++ run_META16s_pipeline_amplicon.sh
@@ -21,7 +21,7 @@ source /local/gensoft2/adm/etc/profile.d/modules.sh
 module purge
 module add Python/2.7.8 FastTree/2.1.8 FLASH/1.2.11 fasta mafft/7.149 bowtie2/2.2.3 blast+/2.2.31 vsearch/1.4.1 AlienTrimmer/0.4.0 fastqc/0.11.2 rdp_classifier/2.11 swarm/2.1.1
 
-/bin/bash $HOME/META16s_pipeline/META16s_pipeline.sh -a $1 -o $2/ -t $3 -b  &> $2/stat_process.txt
+/bin/bash $HOME/META16s_pipeline/META16s_pipeline.sh -a $1 -o $2/ -t $3 -b  &> $2/${3}_stat_process.txt
 """>$PBS_SCRIPT
 PBSID=`qsub $PBS_SCRIPT`
 echo "! Soumission PBS :> JOBID = $PBSID"
